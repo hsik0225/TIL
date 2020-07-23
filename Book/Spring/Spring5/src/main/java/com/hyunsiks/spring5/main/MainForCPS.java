75689bc896eec94836efeb5778e53cd19f35d773
Ever <hsik0225@gmail.com>
2020-07-23 20:02:48 +0900

    [Spring5] logback 의존성 설정 및 logback.xml 설정 파일 추가

--- Book/Spring/Spring5/src/main/java/com/hyunsiks/spring5/main/MainForCPS.java
+++ Book/Spring/Spring5/src/main/java/com/hyunsiks/spring5/main/MainForCPS.java
@@ -14,7 +14,7 @@ public class MainForCPS {
         ChangePasswordService cps = ctx.getBean("changePasswordService", ChangePasswordService.class);
 
         try {
-            cps.changePassword("hsik0225@gmail.com", "1234", "1111");
+            cps.changePassword("hsik0225@gmail.com", "hsik0225", "1111");
             System.out.println("암호를 변경했습니다");
         } catch (MemberNotFoundException e) {
             System.out.println("회원 데이터가 존재하지 않습니다");
